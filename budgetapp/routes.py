2ff5b4a18e1a868389e5acd1cbc85efc67a132c7
David <dechun92@gmail.com>
2017-03-04 22:39:58 -0800

    Add login, verify_login, add_bank routes

--- budgetapp/routes.py
+++ budgetapp/routes.py
@@ -1,3 +1,6 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('login', '/login')
+    config.add_route('verify_login', '/verify_login')
+    config.add_route('add_bank', 'add_bank/{account_type}')
