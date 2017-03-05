d2e56167e92c78d94beaec480a6dae1b57246618
David <dechun92@gmail.com>
2017-03-05 09:43:50 -0800

    Add login template view, add_bank views

--- budgetapp/views/default.py
+++ budgetapp/views/default.py
@@ -3,6 +3,8 @@ from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
+from ..utils.utils import auth_user_to_plaid
+from ..utils.utils import get_access_token_from_auth_response
 from ..models import MyModel
 
 
@@ -14,6 +16,22 @@ def my_view(request):
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
     return {'one': one, 'project': 'budgetapp'}
+
+@view_config(route_name='login', renderer='../templates/logintemplate.jinja2',
+		request_method="GET")
+def login_view(request):
+	return {}
+
+@view_config(route_name='add_bank', renderer='../templates/addbanktemplate',
+		request_method="GET")
+def get_add_bank(request):
+	account_type = request_method.matchdict['account_type']
+
+@view_config(route_name='add_bank', request_method="POST")
+def post_add_bank(request):
+	username = request.params['username']
+	password = request.params['password']
+	
 
 
 db_err_msg = """\
