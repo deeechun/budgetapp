38f7fffd59357f657ae0a24b7d6a29486a6b4b6b
David <dechun92@gmail.com>
2017-03-06 19:46:12 -0800

    Add in transactions and balance getters from connected users

--- budgetapp/utils/utils.py
+++ budgetapp/utils/utils.py
@@ -9,3 +9,16 @@ def auth_user_to_plaid(client, account_type, username, password):
 def get_access_token_from_auth_response(auth_response):
 	return response.json['access_token']
 
+def get_auth_user_balance(client_id, secret, access_token):
+	client = Client(client_id=client_id, secret=secret,
+				access_token=access_token)
+	client_response = client.balance()
+	balance = client_response.json()
+	return balance
+
+def get_auth_user_transactions(client_id, secret, access_token):
+	client = Client(client_id=client_id, secret=secret,
+				access_token=access_token)
+	client_response = client.connect_get()
+	transactions = client_response.json()
+	return transactions
