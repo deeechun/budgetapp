e973205fa1052681086e8715506bd1d27c173a07
David <dechun92@gmail.com>
2017-04-08 18:44:42 -0700

    Import in newly created custom exceptions

--- budgetapp/exceptions/__init__.py
+++ budgetapp/exceptions/__init__.py
@@ -1,2 +1,5 @@
+from .usernameerrors import UsernameExistsError
 from .usernameerrors import UsernameInvalidError
+
+from .emailerrors import EmailExistsError
 from .emailerrors import EmailInvalidError
