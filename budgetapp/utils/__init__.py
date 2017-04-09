28846e082cddd8c6da0f859fb8edcfe7cc5964dd
David <dechun92@gmail.com>
2017-04-09 14:27:29 -0700

    Import utility modules

--- budgetapp/utils/__init__.py
+++ budgetapp/utils/__init__.py
@@ -1,1 +1,18 @@
-#
+from .plaid_client import get_plaid_client_from_settings
+from .registerer import RegisterValidator
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+def includeme(config):
+
+    # Returns settings found in the .ini file specified when running pserve
+    settings = config.get_settings()
+
+    # Create the client used to communicate with API
+    plaid_client = get_plaid_client_from_settings(settings)
+
+    # Adds as a request method to make it accessible through the request
+    config.add_request_method(lambda r:
+                        get_plaid_client_from_settings(settings),
+                        'plaid_client',
+                        reify=True)
