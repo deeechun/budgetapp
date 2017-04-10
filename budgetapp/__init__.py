09d043bcf4086d76e9f0190a1b49599b8b0a2892
David <dechun92@gmail.com>
2017-04-10 15:54:20 -0700

    Add documentation

--- budgetapp/__init__.py
+++ budgetapp/__init__.py
@@ -5,10 +5,12 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
+    # Include files, directories, and packages to the configuration
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
     config.include('.security')
     config.include('.utils')
+    # Scan the following directories/files you've included above
     config.scan()
     return config.make_wsgi_app()
